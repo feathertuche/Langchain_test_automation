93ab0594fd5f143b5e4ae2b4c0440b242091c071
amitesh <amitesh.sahay@yahoo.com>
2025-09-26 00:24:17 +0530

    added feature to upload csv data to Pinecone Index

--- vector/vector.py
+++ vector/vector.py
@@ -2,6 +2,7 @@ import os
 from pinecone import Pinecone
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
+from helper_functions import api_log
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,23 +18,23 @@ class PineconeConn:
 	@classmethod
 	def get_pinecone_connection(cls):
 		try:
-			print("Pinecone connection initiating..")
+			api_log(msg="Pinecone connection initiating..")
 			pine = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 			conn = cls()
 			conn.index = pine.Index(host=os.getenv("PINECONE_HOST"))
 			try:
 			    conn.embedder = SentenceTransformer(conn.pine_embedding_model)
 			except Exception as e:
-				print(f"Failed to load embedding model: {embedding_model}")
+				api_log(msg=f"Failed to load embedding model: {conn.embedder} with {str(e)}")
 				raise e
-			print("Pinecone connection established..")
+			api_log(msg="Pinecone connection established..")
 			return conn
 		except ConnectionRefusedError as cre:
-			print(f"Pinecone connection Failed with {str(cre)}")
+			api_log(msg=f"Pinecone connection Failed with {str(cre)}")
 		
 
 def main():
-	print(get_pinecone_connection())
+	get_pinecone_connection()
  
 if __name__ == '__main__':
 	main()
